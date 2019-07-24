eef9ce87a595988562f8071c9c5ae581ea2a5d38
Lunghi_bruv <nukeri800@gmail.com>
2019-07-24 14:51:08 +0200

    main update

--- get_next_line.c
+++ get_next_line.c
@@ -61,7 +61,7 @@ int				get_next_line(const int fd, char **line)
 	}
 	if (red < 0)
 		return (-1);
-	else if (red == 0 && (st[fd] == NULL || st[fd] == '\0'))
+	else if (red == 0 && (st[fd] == NULL || *st[fd] == '\0'))
 		return (0);
 	return (new_line(st, line, fd, red));
 }
@@ -70,18 +70,19 @@ int				main(int argc, char **argv)
 {
 	int			fd;
 	char		*line;
+	int			p;
 
-	if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
-		return (2);
-	while (get_next_line(fd, &line) == 1)
+	p = 1;
+	while (p <= argc)
 	{
-		ft_putendl(line);
-		free(line);
+		fd = open(argv[p], O_RDONLY);
+		while (get_next_line(fd, &line) == 1)
+		{
+			ft_putendl(line);
+			free(line);
+		}
+		close(fd);
+		p++;
 	}
-	close(fd);
 	return (0);
 }
